e9bcf9611046b8ad9d000b97e64398be8efc0117
raditotev <raditotev@gmail.com>
2015-12-02 10:31:14 +0000

    Added json response to index and show actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -2,6 +2,10 @@ class KittensController < ApplicationController
 
   def index
     @kittens = Kitten.all
+    respond_to do |format|
+      format.html
+      format.json {render json: @kittens}
+    end
   end
 
   def new
@@ -20,6 +24,10 @@ class KittensController < ApplicationController
 
   def show
     @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json {render json: @kitten}
+    end
   end
 
   def edit
